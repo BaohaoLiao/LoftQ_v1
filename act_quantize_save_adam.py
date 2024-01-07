c52f60864c89c959a4f207111e0b1894dd8aadce
baohao <liaobaohao@gmail.com>
2024-01-07 23:11:35 +0100

    add act_quantize_save_batch.py

--- act_quantize_save_adam.py
+++ act_quantize_save_adam.py
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import os
+import math
+import random
 import logging
 import argparse
 import functools
