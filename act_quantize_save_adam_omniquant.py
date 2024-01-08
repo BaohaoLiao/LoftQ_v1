be0fa5ebfb8e1b8f33b919710ec504b35538e702
baohao <liaobaohao@gmail.com>
2024-01-08 20:21:22 +0100

    add act_quantize_save_adam_omniquant.py

--- act_quantize_save_adam_omniquant.py
+++ act_quantize_save_adam_omniquant.py
@@ -271,6 +271,9 @@ def initialize_lora(
 
     ori_lora_layer.weight.data = lora_layer.weight_quantizer(ori_lora_layer.weight.clone()).to(dtype=torch.bfloat16)
 
+    print(gold_layer.weight)
+    print(ori_lora_layer.weight, ori_lora_layer.lora_A.default.weight, ori_lora_layer.lora_B.default.weight)
+
 
 def arg_parse():
     parser = argparse.ArgumentParser(description="Quantize a model")
