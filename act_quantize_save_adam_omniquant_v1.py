e665a6690c66247207638ef50a3911fe97bc54e0
baohao <liaobaohao@gmail.com>
2024-01-21 15:46:19 +0100

    add run_gluepy

--- act_quantize_save_adam_omniquant_v1.py
+++ act_quantize_save_adam_omniquant_v1.py
@@ -187,6 +187,25 @@ def get_dataloader(tokenizer, nsamples, seqlen, seed, dataset_name):
             tar = inp.clone()
             tar[:, :-1] = -100
             trainloader.append((inp, tar))
+    elif dataset_name == "c4":
+        traindata = load_dataset(
+            'allenai/c4', data_files={'train': 'en/c4-train.00000-of-01024.json.gz'}, split='train'
+        )
+
+        random.seed(seed)
+        trainloader = []
+        for _ in range(nsamples):
+            while True:
+                i = random.randint(0, len(traindata) - 1)
+                trainenc = tokenizer(traindata[i]["text"], return_tensors="pt")
+                if trainenc.input_ids.shape[1] >= seqlen:
+                    break
+            i = random.randint(0, trainenc.input_ids.shape[1] - seqlen - 1)
+            j = i + seqlen
+            inp = trainenc.input_ids[:, i:j]
+            tar = inp.clone()
+            tar[:, :-1] = -100
+            trainloader.append((inp, tar))
     return trainloader
 
 @torch.no_grad()
